f8a4d35459e24646d33fcccd51905f56014aa646
TKaiC666 <tkaic666@gmail.com>
2025-04-19 18:10:32 +0800

    modi: test db connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-const HomePage = () => {
-  return <></>;
-};
+import { createClient } from "@/lib/supabase";
 
-export default HomePage;
+export default async function HomePage() {
+  const supabase = await createClient();
+  const { data } = await supabase.from("dev_road_constructions").select();
+  console.log(process.env.SUPABASE_URL);
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
